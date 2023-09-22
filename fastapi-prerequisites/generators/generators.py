d630b18e944a15b4506875e176344e6e82841cc3
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-21 17:36:28 -0700

    pushed generators related code

--- fastapi-prerequisites/generators/generators.py
+++ fastapi-prerequisites/generators/generators.py
@@ -1,1 +1,18 @@
+def infinite_count():
+    num = 1
+    while True:
+        yield num
+        num += 1
 
+count = infinite_count()
+for _ in range(5):
+    print(next(count))
+    
+def countdown(n):
+    while n > 0:
+        yield n
+        n -= 1
+
+# Using the generator to count down from 5
+for number in countdown(5):
+    print(number)
