8b3ee1b4acd3c7ec6c9807f5e81684f81bbbcbb8
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-23 09:55:40 -0700

    Pushed code for Multiple Inheritance

--- fastapi-prerequisites/multipleInheritance/multipleInheritance.py
+++ fastapi-prerequisites/multipleInheritance/multipleInheritance.py
@@ -1,1 +1,33 @@
+# Used two different class to get their properties
 
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+    def getInfo(self):
+        print("Name: "+ self.name + " Age: "+ self.age)
+
+class Football:
+    
+    def __init__(self):
+        pass
+    
+    def playsFootball(self):
+            print("Boy Play football")
+    
+class Boy(Person,Football):
+    def __init__(self,name,age,school):
+        super().__init__(name,age)
+        self.school = school
+
+    
+    def moreInfo(self):
+        print("Name: "+ self.name + " Age: "+ self.age + " School: "+ self.school)
+        
+        
+
+boy = Boy("Joh","20","CSUF")
+boy.getInfo()
+boy.moreInfo()
+boy.playsFootball()
