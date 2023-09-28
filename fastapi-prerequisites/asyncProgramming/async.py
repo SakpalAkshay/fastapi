9c771d86a9beef0fcd23ef9967430b6f1badfb1d
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-09-27 19:10:07 -0700

    Added async code

--- fastapi-prerequisites/asyncProgramming/async.py
+++ fastapi-prerequisites/asyncProgramming/async.py
@@ -1,1 +1,27 @@
+import asyncio
 
+async def main():
+    print("Hello")
+    await asyncio.sleep(5)  # Simulate a non-blocking sleep
+    print("World")
+
+asyncio.run(main())  # Run the asynchronous main function
+
+'''In this example, the await asyncio.sleep(5) line pauses the execution of 
+main for 1 second without blocking the entire program.
+'''
+
+#Another example
+async def fetch_url(url):
+    # Simulate an HTTP request (asynchronous I/O operation)
+    await asyncio.sleep(1)
+    return f"Response from {url}"
+
+async def newMain():
+    urls = ["url1", "url2", "url3"]
+    tasks = [fetch_url(url) for url in urls]
+    responses = await asyncio.gather(*tasks)
+    for response in responses:
+        print(response)
+
+asyncio.run(newMain())
